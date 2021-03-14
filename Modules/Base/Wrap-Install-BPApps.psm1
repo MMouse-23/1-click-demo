3b701a92c2e27c7592d1d597a71a7e40edf254bf
MMouse23 <mgrauwmans@mmouse.nl>
2021-03-14 07:23:16 +0100

    Update Wrap-Install-BPApps.psm1

--- Modules/Base/Wrap-Install-BPApps.psm1
+++ Modules/Base/Wrap-Install-BPApps.psm1
@@ -141,33 +141,7 @@
   $Launch = REST-Generic-BluePrint-Launch -datagen $datagen -datavar $datavar -BPUUID $($blueprint.metadata.uuid) -TaskObject ($blueprintdetail.spec.resources.app_profile_list | where {$_.name -eq "Nutanix"}) -appname "Puppet Open Source"
 
   write-log -message "Puppet Open Source BluePrint Installation Finished" 
-
-  write-log -message "Hadoop BluePrint Installation Started" -sev "CHAPTER"
-
-  Wait-Project-Save-State -datavar $datavar -datagen $datagen -project $project
-  
-  write-log -message "Project UUID is $($project.metadata.uuid)"
-
-  write-log -message "Creating BluePrint"
-
-  $blueprint = REST-Import-Generic-Blueprint-Object -datagen $datagen -datavar $datavar -BPfilepath "$($BlueprintsPath)\Hadoop.json" -Project $project
-
-  write-log -message "Created BluePrint with $($blueprint.metadata.uuid)"
-  write-log -message "Getting newly created blueprint"
-
-  Wait-Project-Save-State -datavar $datavar -datagen $datagen -project $project
-
-  $blueprintdetail = REST-Query-DetailBP -datagen $datagen -datavar $datavar -uuid $($blueprint.metadata.uuid)
-
-  write-log -message "Updating the BP"
-
-  REST-Update-Generic-MarketPlace-Blueprint -datagen $datagen -datavar $datavar -blueprintdetail $blueprintdetail -subnet $subnet -image $image -VMname "Puppet-$($datavar.pocname)"
-
-  Wait-Project-Save-State -datavar $datavar -datagen $datagen -project $project
  
-  $Launch = REST-Generic-BluePrint-Launch -datagen $datagen -datavar $datavar -BPUUID $($blueprint.metadata.uuid) -TaskObject ($blueprintdetail.spec.resources.app_profile_list | where {$_.name -eq "Nutanix"}) -appname "Hadoop"
-
-  write-log -message "Hadoop BluePrint Installation Finished" 
 }
 
 Export-ModuleMember *
